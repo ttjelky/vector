b2b3073d95f1158a9c658354416e6e453c5739ba
ttjelky <bezklinskiyalexey@gmail.com>
2026-02-28 13:34:13 +0200

    Додав папку з компонентами і стилями. Створив компонент тестової кнопки.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
+import ButtonBlack from "./assets/components/buttonblack";
+
 const App = () => {
   return (
     <div>
       <h1>To do list</h1>
+      <ButtonBlack text="Add Task" onClick={() => console.log("Add Task clicked")} />
     </div>
+
   );
 };
 
