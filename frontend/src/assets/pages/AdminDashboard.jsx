6b30e457cc05330b239f5139d292741f25694568
ttjelky <bezklinskiyalexey@gmail.com>
2026-04-14 08:38:03 -0700

    admindashboard, стилі перенесені в окремий файл

--- frontend/src/assets/pages/AdminDashboard.jsx
+++ frontend/src/assets/pages/AdminDashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getProfile } from "../../api";
 import React from "react";
 import NavBar from "../components/NavBar";
+import styles from "../components/styles/admindashboard.module.css";
 
 const Dashboard = () => {
     const [stats, setStats] = useState({
@@ -18,55 +19,25 @@ const Dashboard = () => {
 
     return (
         <NavBar>
-        <div style={styles.dashboard}>
-        <div style={styles.card}>
-            <h3 style={styles.title}>Загальна кількість користувачів</h3>
-            <p style={styles.value}>{stats.total_users}</p>
-        </div>
-        <div style={styles.card}>
-            <h3 style={styles.title}>Активні користувачі</h3>
-            <p style={styles.value}>{stats.active_users}</p>
-        </div>
-        <div style={styles.card}>
-            <h3 style={styles.title}>Нові користувачі</h3>
-            <p style={styles.value}>{stats.new_users}</p>
-        </div>
-        </div>
+            <div>
+                <div className={styles.statscontainer}>
+                    <div className={styles.infocard}>
+                        <h3 className={styles.infotitle}>Загальна кількість користувачів</h3>
+                        <p className={styles.infovalue}>{stats.total_users}</p>
+                    </div>
+                    <div className={styles.infocard}>
+                        <h3 className={styles.infotitle}>Активні користувачі</h3>
+                        <p className={styles.infovalue}>{stats.active_users}</p>
+                    </div>
+                    <div className={styles.infocard}>
+                        <h3 className={styles.infotitle}>Нові користувачі</h3>
+                        <p className={styles.infovalue}>{stats.new_users}</p>
+                    </div>
+                </div>
+            </div>
         </NavBar>
   );
 };
 
-const styles = {
-    dashboard: {
-        display: "flex",
-        gap: "20px",
-        justifyContent: "center",
-        marginTop: "40px",
-        fontFamily: "'Arial', sans-serif",
-        backgroundColor: "#f8f8f8",
-        padding: "20px",
-        borderRadius: "12px",
-    },
-    card: {
-        backgroundColor: "#fff",
-        color: "#111",
-        padding: "30px",
-        borderRadius: "10px",
-        textAlign: "center",
-        minWidth: "150px",
-        boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
-        border: "1px solid #e0e0e0",
-    },
-    title: {
-        fontSize: "16px",
-        marginBottom: "10px",
-        textTransform: "uppercase",
-        color: "#333",
-    },
-    value: {
-        fontSize: "28px",
-        fontWeight: "bold",
-    },
-};
 
 export default Dashboard
