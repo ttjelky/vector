9be9881b4b2f6c075825d118a5f07d0a1574d7b9
dergo0-0 <wallmartbag1990@gmail.com>
2026-04-28 19:21:59 +0300

    перевстановив ноуд(оновив) і зробив назад на лендінгу адапт під новий логотип

--- frontend/src/assets/components/LandingFooter.jsx
+++ frontend/src/assets/components/LandingFooter.jsx
@@ -1,23 +1,19 @@
 import React from "react";
 import styles from "./styles/landingFooter.module.css";
 import Logo from "./static/LogoOnly.svg";
-import LandingBody from "./LandingBody";
 
 const LandingFooter = () => {
     return (
         <footer className={styles.LandingFooter}>
-        <div className={styles.LandingFooterLinks}>
-        <a href="#info">Інфо</a>
-        <a href="#howToStart">Як почати?</a>
-        <a href="#contactUs">Зв’язатися з нами</a>
-        </div>
-        <div className={styles.LandingFooterBottom}>
-            <div className={styles.LandingFooterLogoEmail}>
-                <img src={Logo} className={styles.FooterLogo} />
-                <p className={styles.LandingFooterEmail}>Vectorcommand6742@gmail.com</p>
+            <div className={styles.LandingFooterBottom}>
+                <div className={styles.LandingFooterLogoEmail}>
+                    <img src={Logo} className={styles.FooterLogo} />
+                    <p className={styles.LandingFooterEmail}>Vectorcommand6742@gmail.com</p>
+                </div>
+                <p className={styles.LandingFooterSFLU}>
+                    Турнірне завдання команди Vector <br />Star For Life Ukraine 2026
+                </p>
             </div>
-            <p className={styles.LandingFooterSFLU}>Турнірне завдання команди Vector <br />Star For Life Ukraine 2026</p>
-        </div>
         </footer>
     );
 };
