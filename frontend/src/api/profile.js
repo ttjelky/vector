4b15b5b07d1bc79481f7a1c80720ab80970251ba
Артем <atsvelykh2009@gmail.com>
2026-05-07 17:40:49 +0300

    Revert "Добавив профіль в свою ветку"
    This reverts commit 488ac9bb9b83f74c048833c0a3941056dc4500eb.

--- frontend/src/api/profile.js
+++ frontend/src/api/profile.js
@@ -1,26 +1,25 @@
 import API from "../api";
 
 export const fetchProfile = async () => {
-    const res = await API.get("/users/profile/");
-    return res.data;
+  const res = await API.get("/users/profile/");
+  return res.data;
 };
 
 export const updateProfile = async (data) => {
-    const formData = new FormData();
+  const formData = new FormData();
 
-    formData.append("first_name", data.first_name);
-    formData.append("last_name", data.last_name);
-    formData.append("email", data.email);
+  formData.append("first_name", data.first_name);
+  formData.append("last_name", data.last_name);
 
-    if (data.avatar) {
-        formData.append("avatar", data.avatar);
-    }
+  if (data.avatar) {
+    formData.append("avatar", data.avatar);
+  }
 
-    const res = await API.put("/users/profile/", formData, {
-        headers: {
-            "Content-Type": "multipart/form-data",
-        },
-    });
+  const res = await API.put("/users/profile/", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 
-    return res.data;
+  return res.data;
 };
